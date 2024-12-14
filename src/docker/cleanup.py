b3683f0f70d4f0775d642ac0abb29a1cdfd21871
Your Name <you@example.com>
2024-12-14 07:27:33 -0500

    removed removing folders

--- src/docker/cleanup.py
+++ src/docker/cleanup.py
@@ -13,8 +13,7 @@ def cleanup(clone_dir, results_dir="./results"):
             file_path = os.path.join(results_dir, file)
             if os.path.isfile(file_path):
                 os.remove(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
+
 
 if __name__ == "__main__":
     clone_dir = "./working"
